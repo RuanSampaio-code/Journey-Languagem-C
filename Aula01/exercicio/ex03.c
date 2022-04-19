28153f5930cf1009b9b6bf3f7a765c474c04b393
RuanSampaio-code <ruansampaio001@gmail.com>
2022-04-19 11:08:40 -0300

    ad

---
+++ Aula01/exercicio/ex03.c
@@ -0,0 +1,18 @@
+﻿int main(int argc, char const *argv[])
+{
+//delaração de variaveis    
+int num;
+int num2;
+
+//entrada de dados
+printf("Digite um numero: "); 
+scanf("%d", &num);
+
+//calculor do num ao quadrado
+num2 = num*num;
+
+//saida de dados
+printf("Este numero ao quadrado e igual a %d",num2);
+   
+    return 0;
+}
