0662466019d2e32e5b5daefb38e18aaceb3d7691
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-02 11:15:14 -0300

    ad

---
+++ Aula03 - interações/aula3_interacoes.c
@@ -0,0 +1,57 @@
+﻿// laços
+// execucao predefinida e condiciona
+//tipos : for - predefinida
+// sintaxe:
+// for (iniciação; condição; inc/dec){
+//    comando;
+// }
+//exemplos:
+//int main(int argc, char const *argv[])
+//{
+//    int x; // a variavel pode ser colocada dentro do laço
+//    for (x=1; x<=100; x++){
+//        printf("%d", x);
+//    }
+ //   return 0;
+
+// podemos usar o comando de "," para permitur mais de uma variavel
+//int x
+
+    /* code */
+//WHILE
+//#include <stdio.h>
+/*int main()
+{
+    int x = 1;// d
+    while(x<=100){
+        printf("%d\n", x);
+        x++;
+    }
+    return 0;
+*/
+    //taboada
+int main(int argc, char const *argv[])
+{
+    int num =0;
+    int i=0;
+    printf("digite um numero: ");
+    scanf("d",&num);
+    while (num<=10)
+    {
+    printf("%d * %d = %d", i ,i++, i*i++);
+    }
+    return 0;
+ }
+    
+
+
+
+
+
+
+
+
+//desvios incodicionais
+/* return, break, continue, goto  */
+
+
---
+++ Aula03 - interações/exercicio/ex01.c
@@ -0,0 +1,13 @@
+﻿int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    for (int i = 0; i<=num; i++ ){
+        if (i%5==0)
+            continue;
+        else
+            printf("%d\n", i);        
+    };
+    return 0;
+}
