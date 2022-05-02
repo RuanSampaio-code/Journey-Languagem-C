9733c349abdbd73e6209ccb6da36d406ea6df6fb
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-02 09:14:48 -0300

    ad

---
+++ aula02/exercicios/ex03.c
@@ -0,0 +1,14 @@
+﻿int main(int argc, char const *argv[])
+{
+    //declaração de variaveis
+    int numero;
+    //chamada de dados 
+    printf("Digite um numero e diremos se é par ou imapar: ");
+    scanf("%d",&numero);
+    if(numero%2==0){
+        printf("%d, Este numero é par", numero);
+    }
+    else
+    printf("%d, este numero é impar", numero);
+    return 0;
+}
