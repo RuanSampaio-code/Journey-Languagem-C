1a25657a8153e89c1503f16d4b7a71054bbc2f13
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-08 13:46:55 -0300

    ad

--- Aula03 - interações/exercicio/ex02.c
+++ Aula03 - interações/exercicio/ex02.c
@@ -1,13 +1,13 @@
-﻿int main(int argc, char const *argv[])
-{
-    
-    int i;
-    int soma;
-    for (i=1;i<=500; i+2)
-       
-     
-        printf("%d",i);
-    
+﻿
+int main(int argc, char const *argv[])
 
+{ 
+    int i;
+    int soma = 0;
+    for (i=0;i<=500; i+=2){
+       // printf("%d\n",i);
+        soma = soma +i; 
+   }
+   printf("A soma dos numeros de pares entre 1 a 500 e %d", soma);
     return 0;
 }
