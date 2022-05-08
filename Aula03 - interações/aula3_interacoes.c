bd8753589b39878964c88e7fcfe581ec3372c279
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-08 12:11:02 -0300

    ad

--- Aula03 - interações/aula3_interacoes.c
+++ Aula03 - interações/aula3_interacoes.c
@@ -30,28 +30,24 @@
     return 0;
 */
     //taboada
+    
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int num;
-    int i=0;
-    int n=0;
-    
-
-    printf ("digite um numero: ");
-    scanf("%d", &num);
-    while (n<=num)
-    {
-        while (i<=10)
-        {
-        printf(" %d*%d = %d\n",num,i, num*i);
-        i++;
-         }
-    i++;        
-    }
-    
+    int numero;
+    int i = 0;
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+     while (i<10)
+     {
+         printf("%d * %d = %d \n", numero, i, numero*i);
+         i++;
+     }
+        
+ 
     
     return 0;
- }
+}
     
 
 
---
+++ Aula03 - interações/exercicio/ex02.c
@@ -0,0 +1,13 @@
+﻿int main(int argc, char const *argv[])
+{
+    
+    int i;
+    int soma;
+    for (i=1;i<=500; i+2)
+       
+     
+        printf("%d",i);
+    
+
+    return 0;
+}
