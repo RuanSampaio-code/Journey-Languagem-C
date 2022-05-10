570f8d0dfb6519494a70ce11c22339d4558337a2
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-10 19:53:22 -0300

    Create aula.c

---
+++ aula04 - arrays/aula.c
@@ -0,0 +1,16 @@
+﻿/* 
+arrays sao assim:
+tipo nome[]
+
+uso de indices*/
+
+//imprimindo vetor de tamanho 50:
+int main(int argc, char const *argv[])
+{
+    int vetor[50];
+    for (int i =0; i<50; i++){ 
+        vetor[i] = i;
+        printf("%d\n", vetor[i]);
+    }
+    return 0;
+}
