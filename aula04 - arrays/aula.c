8ab87c88eea5895ba31263e9478d1d6575a0014f
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-11 21:17:03 -0300

    ad

--- aula04 - arrays/aula.c
+++ aula04 - arrays/aula.c
@@ -5,7 +5,7 @@ tipo nome[]
 uso de indices*/
 
 //imprimindo vetor de tamanho 50:
-int main(int argc, char const *argv[])
+/*int main(int argc, char const *argv[])
 {
     int vetor[50];
     for (int i =0; i<50; i++){ 
@@ -14,3 +14,31 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
+*/
+// Mostrando valeres de veotr digitado por laçõs
+int main(int argc, char const *argv[])
+{
+
+    int vet [4];
+    int i;
+
+    // laços para repetição de chamada de numero e armazenar na variavel vet
+    for (i = 0; i <4; i++)
+    {
+        /* code */
+        printf("Digite um numero: ");
+        scanf("%d", &vet[i]);
+    }
+    printf("Este e o vetor digitado ");
+    // saida de dados
+    for (i = 0;i <4; i++)
+    {
+        /* code */
+        printf("%d  ",vet[i]);
+    }
+    
+    
+    return 0;
+}
+
+
