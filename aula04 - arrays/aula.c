ae66cc9bd6af91c00018fda17654d77db3b42da3
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-17 09:32:00 -0300

    ad

--- aula04 - arrays/aula.c
+++ aula04 - arrays/aula.c
@@ -16,16 +16,19 @@ uso de indices*/
 }
 */
 // Mostrando valeres de veotr digitado por laçõs
+ 
+//#include <stdio.h>
+/*
 int main(int argc, char const *argv[])
 {
 
     int vet [4];
     int i;
-
     // laços para repetição de chamada de numero e armazenar na variavel vet
     for (i = 0; i <4; i++)
     {
         /* code */
+        /*
         printf("Digite um numero: ");
         scanf("%d", &vet[i]);
     }
@@ -34,13 +37,49 @@ int main(int argc, char const *argv[])
     for (i = 0;i <4; i++)
     {
         /* code */
-        printf("%d  ",vet[i]);
+/*        printf("%d  ",vet[i]);
     }
     
     
     return 0;
 }
+*/
 
 
 //matrizes vetores multidimensional
+/*
+#include <stdio.h>
 
+int main(){
+    //declarrando matriz 2x2 e variaveis cluna e linha
+    int mat[2][2];
+    int linha, coluna ;
+    printf("Digite os elementos de uma matriz:\n ");
+    
+     for (linha=0; linha <2; linha++)
+         for(coluna =0; coluna<2; coluna++){
+             printf("\nElemento[%d][%d]: ", linha, coluna);
+             scanf("%d", &mat[linha][coluna]);
+         }
+    //saida de dados e imprimindo matriz
+     printf("\n=====matriz imprimindo=====");
+     for (linha=0; linha<2; linha++) 
+         for (coluna = 0; coluna < 2; coluna++)
+         {
+             printf("Elemento [%d][%d] = %d\n", linha, coluna, mat[linha][coluna]);
+         }
+           
+        
+return 0;
+}
+*/
+//Strings 
+
+int main(int argc, char const *argv[])
+{
+    char nome[20];
+    printf("Digite o seu nome: ");
+    scanf("%s", nome);
+    printf("Prazer %s", nome);
+    return 0;
+}
