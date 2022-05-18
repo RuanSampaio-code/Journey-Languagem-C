4e4bcc92231bb3a8c5f1b0f4bdf83117ee69c707
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-18 10:19:36 -0300

    ad

---
+++ aula04 - arrays/exercicios/ex001.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{   
+    char palavra[100];
+    int numvogais = 0;
+    int i;
+    printf("Digite uma palavra: ");
+    scanf(" %s", &palavra);
+    
+
+    for (i = 0; i < palavra[15]; i++)
+    {
+    if(palavra[i]== 'A' || palavra[i]== 'a' || 
+       palavra[i]== 'E' || palavra[i]== 'e' || 
+       palavra[i]== 'I' || palavra[i]== 'i' ||
+       palavra[i]== 'O' || palavra[i]== 'o' || 
+       palavra[i]== 'U' || palavra[i]== 'u' );{
+       numvogais++;
+    }
+    printf("%d", numvogais);
+    
+    return 0;
+
+}
