f459c650c4f81178928c7c6fa47a1246bf090b8e
RuanSampaio-code <ruansampaio001@gmail.com>
2022-05-20 10:07:03 -0300

    ad

--- aula04 - arrays/exercicios/ex001.c
+++ aula04 - arrays/exercicios/ex001.c
@@ -1,5 +1,5 @@
 ﻿
-
+/*
 #include<stdio.h>
 #include<string.h>
 
@@ -10,33 +10,39 @@ int main () {
 
     printf("Digite o nome:");  
     scanf("%s",palavra);
-    for ( int i = 0; i < strlen(palavra); i++)
-    {
-        for (int j = 0; j < strlen(vogais); j++)
-        {
-            
-        if (palavra[i]==vogais[j])
-        
-            numvogais++;
-        
-            
-        }
-        
-        
-    }
+   
     
-    printf("total de vogais: %d",numvogais);
-return 0;
-  }
+
+  
 //for (int i=0; nome[i]!='\0'; i++){
-    /*for (int i=0; i<strlen(palavra); i++){
+    for (int i=0; i<strlen(palavra); i++){
         if(palavra[i]=='A'|| palavra[i]=='a' || 
             palavra[i]=='E'|| palavra[i]=='e' ||
             palavra[i]=='I'|| palavra[i]=='i' || 
             palavra[i]=='O'|| palavra[i]=='o' ||
             palavra[i]=='U'|| palavra[i]=='u' )
             numvogais++;
-}  */
-/*printf("total de vogais: %d",numvogais);
+}  
+printf("total de vogais: %d",numvogais);
 return 0;
   }*/
+  #include<stdio.h>
+#include<string.h>
+
+int main () {  
+char nome[80];
+char vogais[] ={"aeiouAEIOU"};
+int cont=0;
+printf("Digite o nome:");  
+scanf("%s",nome);  
+
+
+for (int i=0; i<strlen(nome); i++){
+  for (int j=0; j<strlen(vogais); j++){
+      if(nome[i]==vogais[j])
+       cont++;
+      }
+}  
+printf("total de vogais %d",cont);
+return 0;
+}
