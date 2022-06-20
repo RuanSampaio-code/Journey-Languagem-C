6aa40ad565d1b6590831c12d8ceba4447f1edc3f
RuanSampaio-code <ruansampaio001@gmail.com>
2022-06-20 10:07:38 -0300

    ad

---
+++ Aula08 - Estruturas/aula.c
@@ -0,0 +1,28 @@
+﻿// struct 
+
+// gravar 2 notas de um aluno e imprimir media - usando struct
+
+#include <stdio.h>
+// declaração da struct
+struct Alunos
+{
+    int matricula;
+    float nota1;
+    float nota2;
+
+}aluno; // declaração da struct
+
+int main()
+{
+    printf("Matricula: ");
+    scanf ("%d", &aluno.matricula);
+    printf("Nota1: ");
+    scanf("%f", &aluno.nota1);
+    printf("Nota2: ");
+    scanf("%f", &aluno.nota2);
+    float media;
+    media = (aluno.nota1 + aluno.nota2)/2;
+    return 0;
+    printf("media: %f", media);
+}
+
