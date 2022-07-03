4a6df8f9401667e1d04ee4a8c20717ddcc10a5dd
RuanSampaio-code <ruansampaio001@gmail.com>
2022-07-03 18:55:56 -0300

    ad

--- prova3/avaliação3.c
+++ prova3/avaliação3.c
@@ -6,16 +6,18 @@ struct Clientes
     char prato [100];
     float valor;
 };
+
 struct Clientes pedido;
 
 void imprimir (struct Clientes ped){
-    printf("===INFORMACOES DO PEDIDO===");
-    printf("\nCliente: %s", ped.nome);
-    printf("\nPrato: %s", ped.prato);
-    printf("\nValor em R$: %.2f", ped.valor);
+    printf("\n===INFORMACOES DO PEDIDO===");
+    printf("nCliente: %s", ped.nome);
+    printf("Prato: %s", ped.prato);
+    printf("Valor em R$: %.2f", ped.valor);
 }
 
 void altera (struct Clientes *ped){
+    printf("\n ======Alterando pedido:======");
     printf("\nCliente: ");
     scanf("%s", &(*ped).nome);
     printf("Prato: ");
@@ -24,10 +26,20 @@ void altera (struct Clientes *ped){
     scanf("%f", &(*ped).valor);
 }
 
+void adcionar(struct Clientes ped){
+    printf("\n ======Adicionando novo pedido=====");
+    printf("O que mais desejaria? ");
+    fgets(&pedido.prato,100,stdin);
+    printf("Valor em reais: ");  
+    scanf("%f", &pedido.valor);
+    imprimir(pedido);
+
+}
+
 int main()
 {
-   
-    printf("====== Bem vindo ao Restaurante =====");   
+    int opcao = 0;
+    printf("=========Bem vindo ao Restaurante=====");   
     printf("\nDigite o nome do cliente: ");
     fgets(&pedido.nome,100,stdin);
     printf("Qual o seu pedido? ");
@@ -35,6 +47,17 @@ int main()
     printf("Valor em reais: ");  
     scanf("%f", &pedido.valor);
     imprimir(pedido);
+
+    
+    printf("\nVoce gostaria de alterar o pedido? aperte: \n(1) - Alterar \n(2) - Adicionar \n(3) - Finalizar  \n");
+    scanf("%d", &opcao);
+    if (opcao==1)
+        altera(&pedido);
+    else if (opcao ==2)
+        adcionar(pedido);
+    else if (opcao ==3)
+        printf("Pedido finalizado, Obrigado pela Compra!!");
+
     return 0;
 
 }
