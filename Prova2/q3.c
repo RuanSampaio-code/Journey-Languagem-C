efb17d83ca0d2913a8735f19ba50bc9370ac0dec
RuanSampaio-code <ruansampaio001@gmail.com>
2022-07-05 22:11:06 -0300

    ad

--- Prova2/q3.c
+++ Prova2/q3.c
@@ -1,1 +1,17 @@
-﻿
+﻿#include <stdio.h>
+
+int main()
+{
+    int termos;
+    char palavra;
+   
+    printf("Determine uma quantidade de vezes para ler uma palavrar: ");
+    scanf("%d",&termos);
+
+    for (int i = 0; i <= termos; i++)
+    {
+        printf("\nDigite uma palavra:");
+        fgets(palavra, i,stdin);
+    }
+    return 0;
+}
