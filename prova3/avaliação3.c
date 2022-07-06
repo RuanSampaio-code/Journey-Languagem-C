033554a797351fed9e69c2e463dda293352b4a5d
RuanSampaio-code <ruansampaio001@gmail.com>
2022-07-05 23:04:36 -0300

    ad

--- prova3/avaliação3.c
+++ prova3/avaliação3.c
@@ -1,5 +1,4 @@
 ﻿#include <stdio.h>
-
 struct Clientes
 {
     char nome [100];
@@ -33,16 +32,16 @@ void adcionar(struct Clientes ped){
     printf("Valor em reais: ");  
     scanf("%f", &pedido.valor);
     imprimir(pedido);
-
 }
 
 int main()
 {
     int opcao = 0;
-    printf("=========Bem vindo ao Restaurante=====");   
+    int pagamento =0;
+    printf("=========Bem vindo ao Restaurante =====");   
     printf("\nDigite o nome do cliente: ");
     fgets(&pedido.nome,100,stdin);
-    printf("Qual o seu pedido? ");
+    printf("Qual o seu pedido?  ");
     fgets(&pedido.prato,100,stdin);
     printf("Valor em reais: ");  
     scanf("%f", &pedido.valor);
@@ -56,7 +55,14 @@ int main()
     else if (opcao ==2)
         adcionar(pedido);
     else if (opcao ==3)
-        printf("Pedido finalizado, Obrigado pela Compra!!");
+        printf("Pedido finalizado, Qual a forma de pagamento");
+        printf("Qual a forma de pagamento? \n(1) - cartão de débido \n(2) - Cartão de crédito \n(3) - Boleto/Dinheiro/Pix \n");
+        scanf("%d", &pagamento);
+        if ( pagamento==1 || pagamento==2 || pagamento==3)
+            printf(" Pagamento realizado, Obrigado pela preferencia, Volte sempre!");
+            /* code */
+        
+        
 
     return 0;
 
