dcc3019c051dbecf63b96a4a7321892944ba7210
RuanSampaio-code <ruansampaio001@gmail.com>
2022-10-05 14:29:50 -0300

    ad

--- prova3/avaliação3.c
+++ prova3/avaliação3.c
@@ -9,27 +9,28 @@ struct Clientes
 struct Clientes pedido;
 
 void imprimir (struct Clientes ped){
-    printf("\n===INFORMACOES DO PEDIDO===");
-    printf("nCliente: %s", ped.nome);
-    printf("Prato: %s", ped.prato);
-    printf("Valor em R$: %.2f", ped.valor);
+    printf("\n===== |INFORMACOES DO PEDIDO| =====\n");
+    printf("\nCliente: %s", ped.nome);
+    printf("\nPrato: %s", ped.prato);
+    printf("\nValor em R$: %.2f", ped.valor);
 }
 
 void altera (struct Clientes *ped){
-    printf("\n ======Alterando pedido:======");
-    printf("\nCliente: ");
+    printf("\n ======Alterando pedido:======\n");
+    printf("Cliente: ");
     scanf("%s", &(*ped).nome);
     printf("Prato: ");
     scanf("%s", &(*ped).prato);
     printf("Valor: ");
     scanf("%f", &(*ped).valor);
+    imprimir(pedido);
 }
 
 void adcionar(struct Clientes ped){
-    printf("\n ======Adicionando novo pedido=====");
-    printf("\nO que mais desejaria? ");
-    fgets(&pedido.prato,100,stdin);
-    printf("Valor em reais: ");  
+    printf("\n ======Adicionando novo pedido=====\n");
+    printf("\nNovo Prato: ");
+    scanf("%s", &pedido.prato); 
+    printf("\nValor em reais: ");  
     scanf("%f", &pedido.valor);
     imprimir(pedido);
 }
@@ -38,7 +39,7 @@ int main()
 {
     int opcao = 0;
     int pagamento =0;
-    printf("=========Bem vindo ao Restaurante =====");   
+    printf("========= || Bem vindo ao nosso Restaurante! || =====");   
     printf("\nDigite o nome do cliente: ");
     fgets(&pedido.nome,100,stdin);
     printf("Qual o seu pedido?  ");
@@ -47,19 +48,19 @@ int main()
     scanf("%f", &pedido.valor);
     imprimir(pedido);
 
-    
-    printf("\nVoce gostaria de alterar o pedido? aperte: \n(1) - Alterar \n(2) - Adicionar \n(3) - Finalizar  \n");
+    printf("\n\n====================================");
+    printf("\n\nO que voce gostaria? Digite: \n(1) - Alterar o pedido \n(2) - Adicionar mais um pedido \n(3) - Finalizar  \n");
     scanf("%d", &opcao);
     if (opcao==1)
         altera(&pedido);
     else if (opcao ==2)
         adcionar(pedido);
     else if (opcao ==3)
-        printf("Pedido finalizado, Qual a forma de pagamento");
-        printf("Qual a forma de pagamento? \n(1) - Cartao de debido \n(2) - Cartao de credito \n(3) - Boleto/Dinheiro/Pix \n");
+        printf("\nPedido finalizado.");
+        printf("\nQual a forma de pagamento? \n(1) - Cartao de debido \n(2) - Cartao de credito \n(3) - Boleto/Dinheiro/Pix \n");
         scanf("%d", &pagamento);
         if ( pagamento==1 || pagamento==2 || pagamento==3)
-            printf(" Pagamento realizado, Obrigado pela preferencia, Volte sempre!");
+            printf("Pagamento realizado, Obrigado pela preferencia, Volte sempre!");
     return 0;
 
 }
